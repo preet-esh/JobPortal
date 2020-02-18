25743939924ae66ba3b9cd50226d87066cab3f55
pritesh <pritesh.bhati59@gmail.com>
2020-02-18 13:58:16 +0530

    add connection file

--- server/controller/UserController.js
+++ server/controller/UserController.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const keys = require('../config/keys');
 const mysqlConnection=require('../cnct');
 
 exports.create= (req, res) => {
